191a97204882abc415c6817c273bf5fe31c4e383
Tim König <tim.koenig@stud.uni-bamberg.de>
2022-11-18 13:58:47 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Today from "./lib/Today.svelte";
+
+  const adventCalendar = () => {
+    alert("test")
+  };
+
+
+</script>
+
+<main>
+  <div>
+    <h1>Mogli</h1>
+  </div>
+
+  <div class="card">
+    <button on:click={adventCalendar}>
+      Zum Adventskalender
+    </button>
+    <Today />
+  </div>
+
+</main>
+
+<style>
+  main div h1 {
+    font-size: 60px;
+  }
+
+</style>
