f619c48d26b17544ed3b23cdfb28a33f3af98eec
Tim König <tim.koenig@stud.uni-bamberg.de>
2022-11-22 14:16:08 +0100

    Added calendar and more comments

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -17,3 +17,25 @@ const router = async () => {
 };
 
 document.addEventListener("DOMContentLoaded", () => router());
+
+const makeDoorsAvailable = () => {
+    // check date and make all doors up until that date available
+}
+
+const handleDoorClick = (door, doorNumber) => {
+    console.debug("Door with number " + doorNumber + " clicked!")
+    if (door.classList.contains("available")) { // additionally check for date
+        console.log("Door " + doorNumber + " is available.")
+    } else {
+        alert("Dieses Türchen kannst du noch nicht öffnen!")
+    }
+    // check date again and check if door available
+    // if true, open the specific door in this tab
+    // if false, alert that door is not to be opened yet!
+}
+
+const doors = document.getElementsByClassName("advent-door");
+
+for (let i = 0; i < doors.length; i++) {
+    doors[i].addEventListener("click", () => handleDoorClick(doors[i], i+1))
+}
